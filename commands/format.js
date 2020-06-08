cf31d0a0d70a3a5f698a24f5ebbd6fe131791e6a
Oluwamayowa Esan <esanoluwamayowa@gmail.com>
2020-06-08 11:11:18 -0700

    oops other way around

--- commands/format.js
+++ commands/format.js
@@ -2,7 +2,7 @@ const { Permissions } = require('discord.js');
 module.exports = {
     action(args) {
         const message = args[args.length - 1];
-        message.channel.send(message.content.replace(/_/g, ' '));
+        message.channel.send(message.content.replace(/ /g, '_'));
     },
     permission_level: Permissions.FLAGS.MANAGE_GUILD
 }
