b7842be9450b7b35827138326ae62b19f84e157f
Oluwamayowa Esan <esanoluwamayowa@gmail.com>
2020-06-08 10:59:54 -0700

    ability to add message content to bulletin

--- commands/createbulletin.js
+++ commands/createbulletin.js
@@ -6,10 +6,15 @@ module.exports = {
         try {
             const message = args[args.length - 1];
             const title = args[1].replace(/_/g, ' '), description = args[2].replace(/_/g, ' '), color = args[3];
+            let additionalContent;
+            if(args.length === 6) additionalContent = args[4].replace(/_/g, ' ');
+            console.log(args.length);
             const channel = message.mentions.channels.first();
+            if(!channel)
+                return message.reply('please specify a channel to send this bulletin to!');
 
             const embed = createEmbed({ title, color, description });
-            await channel.send(embed);
+            additionalContent ? await channel.send(additionalContent, {embed}) : await channel.send(embed);
             message.reply(`the **${title}** bulletin has been added in ${channel}.`);
         } catch (e) { console.log(e); }
     },
