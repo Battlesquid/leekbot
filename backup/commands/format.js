a897d7caa2d8313688f958195b7769a94c9b0887
battlesqui_d <oesan@asu.edu>
2020-08-18 14:25:55 -0700

    remove backup folder

--- backup/commands/format.js
+++
@@ -1,8 +0,0 @@
-const { Permissions } = require('discord.js');
-module.exports = {
-    action(args) {
-        const message = args[args.length - 1];
-        message.channel.send(message.content.replace(/ /g, '_'));
-    },
-    permission_level: Permissions.FLAGS.MANAGE_GUILD
-}
--- backup/events/guildCreate.js
+++
@@ -1,7 +0,0 @@
-const firebase = require('../util/firebase.js');
-module.exports = (bot, guild) => {
-    firebase.database.ref(guild.id.toString()).set({
-        "channels": 0,
-        "log_channel": 0
-    });
-} 
